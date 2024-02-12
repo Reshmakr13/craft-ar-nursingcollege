0318ac73a5b4c3ded2dfa166411139f217acc582
Reshma K R <reshmakanjoorparambil@gmail.com>
2024-02-12 16:34:34 +0530

    commit after

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,35 +1,35 @@
 <x-easyadmin::guest-layout>
 
 
-<div class="items-center">
+<div>
 
     <header>
-        <nav class="flex md:flex-col mb-6 md:mb-0">
+        <nav class="flex md:flex-col mb-6 md:mb-0 md:w-full ">
             <div class="md:flex md:flex-row hidden md:block md:bg-primary md:w-full p-4 ">
-                <div class="flex flex-row">
-                    <div class="flex ml-10 items-center">
+                <div class="flex flex-row ">
+                    <div class="flex ml-20 items-center">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="h-4 w-4 fill-current text-white mt-1 ">
                         <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
                         <path d="M280 0C408.1 0 512 103.9 512 232c0 13.3-10.7 24-24 24s-24-10.7-24-24c0-101.6-82.4-184-184-184c-13.3 0-24-10.7-24-24s10.7-24 24-24zm8 192a32 32 0 1 1 0 64 32 32 0 1 1 0-64zm-32-72c0-13.3 10.7-24 24-24c75.1 0 136 60.9 136 136c0 13.3-10.7 24-24 24s-24-10.7-24-24c0-48.6-39.4-88-88-88c-13.3 0-24-10.7-24-24zM117.5 1.4c19.4-5.3 39.7 4.6 47.4 23.2l40 96c6.8 16.3 2.1 35.2-11.6 46.3L144 207.3c33.3 70.4 90.3 127.4 160.7 160.7L345 318.7c11.2-13.7 30-18.4 46.3-11.6l96 40c18.6 7.7 28.5 28 23.2 47.4l-24 88C481.8 499.9 466 512 448 512C200.6 512 0 311.4 0 64C0 46 12.1 30.2 29.5 25.4l88-24z"/></svg>
                         <p class="text-white ml-2 font-light font-gothic ">0480-2800200</p>
                     </div>
                 
-                    <div class="flex ml-32 items-center">
+                    <div class="flex ml-20 items-center">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="h-5 w-4 fill-current text-white mt-1 ml-48">
                         <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
                         <path d="M64 112c-8.8 0-16 7.2-16 16v22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1V128c0-8.8-7.2-16-16-16H64zM48 212.2V384c0 8.8 7.2 16 16 16H448c8.8 0 16-7.2 16-16V212.2L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64H448c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128z"/></svg> 
                         <p class="text-white ml-2 font-light font-questrial">nursingschool@craftivf.com</p>
                     </div>
                 
-                    <div class="flex space-x-8  ml-96 pl-96 ">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" class="h-5 w-4 fill-current text-white mt-1 ml-28">
+                    <div class="flex md:ml-40 items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" class="h-5 w-4 fill-current text-white mt-1 ">
                         <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
                         <path d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z"/></svg>
                         
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="h-5 w-4 fill-current text-white mt-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="h-5 w-4 fill-current text-white mt-1 ml-8">
                         <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
                         <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/></svg>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="h-5 w-4 fill-current text-white mt-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="h-5 w-4 fill-current text-white mt-1 ml-8">
                         <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
                         <path d="M100.3 448H7.4V148.9h92.9zM53.8 108.1C24.1 108.1 0 83.5 0 53.8a53.8 53.8 0 0 1 107.6 0c0 29.7-24.1 54.3-53.8 54.3zM447.9 448h-92.7V302.4c0-34.7-.7-79.2-48.3-79.2-48.3 0-55.7 37.7-55.7 76.7V448h-92.8V148.9h89.1v40.8h1.3c12.4-23.5 42.7-48.3 87.9-48.3 94 0 111.3 61.9 111.3 142.3V448z"/></svg>
                     </div>
@@ -68,8 +68,8 @@
     </header>
 
     <div class="md:flex flex-row md:ml-32 md:mb-20">
-        <div class="mt-10 md:mt-48">
-            <div class="ml-4">
+        <div class="mt-4 md:mt-48 md:w-1/2 md:mr-12">
+            <div class="ml-4 md:mr-48">
                 <h class="text-2xl  font-bold text-black md:text-6xl font-gothic">Contact Us</h>
                 <p class="md:hidden font-normal  text-xs mt-6 md:text-xl font-gothic">Feel free to contact us any time. We will get back to you as we can!</p>
                 <p class="hidden md:block font-normal  text-xs mt-6 md:text-xl font-gothic">Feel free to contact us any time. We will get<br> back to you as we can!</p>
@@ -122,30 +122,30 @@
         </div>
         
 
-    <div class="mt-48 relative ml-4 mb-40 flex justfy-end items-center md:ml-96">
-        <div class=" bg-primary h-96 py-80  px-12 md:px-60 absolute right-0 md:py-96 md:-mr-80 "></div>
-        <div class="bg-black w-96 md:w-full mr-6 z-10 md:pr-12 md:pl-20">
-            <div class="ml-8 md:-ml-8 mt-8 ">
-                <div class=""><h class="text-white text-sm font-bold font-inter md:text-3xl">Info</h></div>
-                <div class="flex mt-16 items-center">
-                    <p class="text-white text-xs font-bold font-gothic md:text-2xl">Email</p>
-                    <p class="text-white text-xs font-gothic ml-28 md:text-sm">craftnursingcollege.com</p>
-                </div>
-                <div class="flex mt-16 items-center">
-                <p class="text-white text-xs font-bold font-gothic md:text-2xl">Phone No.</p>
-                <p class="text-white text-xs font-gothic ml-16 md:text-sm">+91 9526986000</p>
-                </div>
-                <div class="flex mt-16 ">
-                <p class="text-white text-xs font-bold font-gothic md:text-2xl">Address</p>
-                <div class="flexflex-col mb-16">
-                    <p class="text-white text-xs font-inter mb-4 ml-16 md:text-sm">CRAFT HOSPITAL AND RESEARCH<br> CENTRE,VIII/77/ 2,3,4, Kodungallur P.O,<br> Chanthappura,Kodungallur, Thrissur, Kerala</p>
-                    <p class="text-white text-xs font-inter mb-4 ml-16 md:text-sm">CRAFT Multispeciality Centre5th & 6th Floor,<br> Imperial Amity,Chalikkavattam, NH66 <br>BypassKochi 682032, kerala</p>
-                </div> 
+        <div class="mt-48 relative ml-4 mb-40 flex flex-reverse items-center md:w-1/2">
+            <div class=" bg-primary h-96 py-80  px-12 md:px-0 absolute right-0 md:py-96 w-1/2"></div>
+            <div class="bg-black w-96 md:w-2/3 mr-6 z-10">
+                <div class="ml-4 mt-8">
+                    <div class=""><h class="text-white text-sm font-bold font-inter md:text-3xl">Info</h></div>
+                    <div class="flex mt-16 items-center">
+                        <p class="text-white text-xs font-bold font-gothic md:text-2xl">Email</p>
+                        <p class="text-white text-xs font-gothic ml-24 md:text-sm">craftnursingcollege.com</p>
+                    </div>
+                    <div class="flex mt-16 items-center">
+                    <p class="text-white text-xs font-bold font-gothic md:text-2xl">Phone No.</p>
+                    <p class="text-white text-xs font-gothic ml-12 md:text-sm">+91 9526986000</p>
+                    </div>
+                    <div class="flex mt-16 ">
+                        <p class="text-white text-xs font-bold font-gothic md:text-2xl">Address</p>
+                        <div class="flexflex-col mb-16">
+                        <p class="text-white text-xs font-inter mb-4 ml-16 md:text-sm">CRAFT HOSPITAL AND RESEARCH<br> CENTRE,VIII/77/ 2,3,4, Kodungallur P.O,<br> Chanthappura,Kodungallur, Thrissur,<br> KeralaPh: +91 6282396564</p>
+                        <p class="text-white text-xs font-inter mb-4 ml-16 md:text-sm">CRAFT Multispeciality Centre 5th & <br>6th Floor,Imperial Amity,Chalikkavattam,<br>NH66 Bypass Kochi 682032,<br> Kerala Ph:+91 9526986000</p>
+                        </div> 
+                    </div>
                 </div>
             </div>
         </div>
     </div>
- </div>
     
 
 
